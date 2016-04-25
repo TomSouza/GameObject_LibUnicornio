837e3c1d8d67ac2b969b4c9dc324beac388f2027
Thomas Souza <thomas.aes.31@hotmail.com>
2016-04-24 22:45:57 -0300

    DOC(movement): Documantação da classe
    Documenta a classe os metodos e os enums.

--- Movement.h
+++ Movement.h
@@ -1,16 +1,29 @@
 #pragma once
 #include "libUnicornio.h"
 
+/// <summary>
+/// Classe responsável por controlar a movimentação do objeto de jogo
+/// </summary>
 class Movement
 {
 public:
     Movement();
     ~Movement();
 
-    void move(float &pos_x, float &posy, float speed);
+    /// <summary>
+    /// Move o objeto
+    /// </summary>
+    /// <param name="pos_x"> Posição atual do eixo X do objeto </param>
+    /// <param name="pos_y"> Posição atual do eixo Y do objeto </param>
+    /// <param name="speed"> Velocidade que será utilizada no incremento da posição </param>
+    void move(float &pos_x, float &pos_y, float speed);
 
 protected:
-    enum defautl_comands {
+
+    /// <summary>
+    /// Teclas padrões que serão utilizadas para movimento
+    /// </summary>
+    enum default_commands {
         up = TECLA_W,
         down = TECLA_S,
         left = TECLA_A,
