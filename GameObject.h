00a969e3809ac33806e3ba8e1522538114ced743
Thomas Souza <thomas.aes.31@hotmail.com>
2016-04-24 22:43:48 -0300

    DOC(gameObject): Adiciona documentação a todos metodos

--- GameObject.h
+++ GameObject.h
@@ -2,26 +2,82 @@
 #include "libUnicornio.h"
 #include "Movement.h"
 
+/// <summary>
+/// Classe responsável por controlar funções básicas de um objeto de jogo
+/// </summary>
 class GameObject
 {
 public:
     GameObject();
     ~GameObject();
 
+    /// <summary>
+    /// Enumerador de tipos de movimento que podem ser livres (allDirections)
+    /// ou como um jogo de plataforma (side2side)
+    /// </summary>
     enum movementType {
         allDirections, side2side
     };
 
-    void initialize(string sprite_name, float initial_x, float initial_y, bool has_colision, bool has_movement, bool has_animation);
+    /// <summary>
+    /// Inicializa um objeto de jogo com informações básicas sobre ele
+    /// </summary>
+    /// <param name="sprite_name"> Nome do spritesheet já carregado que será usado no objeto </param>
+    /// <param name="initial_x"> Posição inicial do objeto no eixo X </param>
+    /// <param name="initial_y"> Posição inicial do objeto no eixo Y </param>
+    /// <param name="has_colision"> Determina se o objeto terá colisor; por padrão não possuirá </param>
+    /// <param name="has_movement"> Determina se o objeto terá movimento; por padrão não possuirá </param>
+    /// <param name="has_animation"> Determina se o objeto terá animação; por padrão não possuirá </param>
+    void initialize(
+            string sprite_name, 
+            float initial_x, 
+            float initial_y, 
+            bool has_colision = false, 
+            bool has_movement = false, 
+            bool has_animation = false
+    );
 
+    /// <summary>
+    /// Determina se o objeto terá ou não um colisor
+    /// </summary>
+    /// <param name="state"> Estado de atividade do colisor </param>
     void setColisor(bool state);
+
+    /// <summary>
+    /// Determina se o objeto terá movimento que por padrão será para todos os lados se estiver ativo
+    /// </summary>
+    /// <param name="state"> Estado de atividade do movimento </param>
+    /// <param name="type"> Tipo de movimeto que o objeto terá </param>
     void setMovement(bool state , movementType type = allDirections);
+
+    /// <summary>
+    /// Determina se o objeto terá ou não uma animação
+    /// </summary>
+    /// <param name="state"> Estado de atividade da animação </param>
     void setAnimation(bool state);
 
+    /// <summary>
+    /// Desenha o obejto na tela do jogo; também possui controle sobre 
+    /// a verificação do movimento e das colisões
+    /// </summary>
     void draw();
+
+    /// <summary>
+    /// Muda a animação que o objeto está utilizando
+    /// </summary>
+    /// <param name="animation_line"> Faixa de animação que será utilizada </param>
     void changeAnimation(int animation_line);
+
+    /// <summary>
+    /// Determina a velocidade de animação do objeto
+    /// </summary>
+    /// <param name="speed"> Velocidade que será passsada para a animação </param>
     void setAnimationSpeed(float speed);
 
+    /// <summary>
+    /// Determina a velocidade de movimento do objeto
+    /// </summary>
+    /// <param name="speed_to_set"> Velocidade que será passsada para o objeto </param>
     void setSpeed(float speed_to_set);
 
 protected:
