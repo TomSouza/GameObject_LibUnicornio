24d74b67c78694a7c6ed79435484ae4951b0d2f8
Thomas Souza <thomas.aes.31@hotmail.com>
2016-05-01 01:51:14 -0300

    FEA(movement.interface): Animação nos movimentos
    Adiciona metodo que recebe o valor das animações que deverão ser
    passadas para seus respectivos movimentos. Adiciona strutura que recebera
    as informações do numero das animações para os movimentos. Adiciona array
    que define se alguma animação sera invertida.
    Modifica parametros do metodo move para receber o sprite do objeto por
    ponteiro e o tipo do movimento.

--- Movement.h
+++ Movement.h
@@ -16,7 +16,20 @@ public:
     /// <param name="pos_x"> Posição atual do eixo X do objeto </param>
     /// <param name="pos_y"> Posição atual do eixo Y do objeto </param>
     /// <param name="speed"> Velocidade que será utilizada no incremento da posição </param>
-    void move(float &pos_x, float &pos_y, float speed);
+    /// <param name="body"> Sprite do objeto </param>
+    void move(float &pos_x, float &pos_y, float speed, Sprite &body, int motionType );
+
+    /// <summary>
+    /// Ajusta qual animação será usada pelo movimento. Animações que receberem valor zero
+    /// não serão modificadas, já que o valor inicial de todas é zero.
+    /// </summary>
+    /// <param name="set_stopped"> Numero da animação do objeto parado </param>
+    /// <param name="set_up"> Numero da animação para cima </param>
+    /// <param name="set_down"> Numero da animação para baixo </param>
+    /// <param name="set_left"> Numero da animação para esquerda </param>
+    /// <param name="set_right"> Numero da animação para direita </param>
+    /// <param name="invert"> Array que indica qual animação sera invertida </param>
+    void animationAdjustment(int set_stopped, int set_up, int set_down, int set_left, int set_right, bool *invert);
 
 protected:
 
@@ -30,5 +43,17 @@ protected:
         right = TECLA_D
     };
 
+    struct animateMove {
+        int stopped = 0;
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+    };
+
+    animateMove animations;
     float speed;
+
+    const int invert_size = 4;
+    bool inversions[4] = { };
 };
