6a909bf90e7be7e543110df3c67d6a34e27d9352
Thomas Souza <thomas.aes.31@hotmail.com>
2016-05-01 02:13:15 -0300

    FEA(movement.implementacao): Movimento e animações
    Adiciona metodo que define valores para as animações utilizadas pelos
    movimentos.
    Modifica metodo move para verificar o tipo de movimentação utilizada e
    implementa a animação para cada movimento, verificando tambem se ele
    precisa ser invertido ou não.

--- Movement.cpp
+++ Movement.cpp
@@ -8,25 +8,112 @@ Movement::~Movement()
 {
 }
 
-void Movement::move(float &pos_x, float &pos_y, float speed)
+void Movement::move(float &pos_x, float &pos_y, float speed, Sprite &body, int motionType)
 {
     gDebug.depurar("pos X Mov", pos_x);
     gDebug.depurar("pos Y Mov", pos_y);
     gDebug.depurar("speed Mov", speed);
+    gDebug.depurar("Tipo", motionType);
 
-    if (gTeclado.segurando[up]) {
-        pos_y -= speed;
+    bool advance = false;
+
+    switch (motionType) {
+        case 0: // All Directions
+
+            if (gTeclado.segurando[up]) {
+                pos_y -= speed;
+                body.setAnimacao(animations.up);
+                body.setInverterY(inversions[0]);
+                advance = true;
+            }
+
+            if (gTeclado.segurando[down]) {
+                pos_y += speed;
+                body.setAnimacao(animations.down);
+                body.setInverterY(inversions[1]);
+                advance = true;
+            }
+
+            if (gTeclado.segurando[left]) {
+                pos_x -= speed;
+                body.setAnimacao(animations.left);
+                body.setInverterX(inversions[2]);
+                advance = true;
+            }
+
+            if (gTeclado.segurando[right]) {
+                pos_x += speed;
+                body.setAnimacao(animations.right);
+                body.setInverterX(inversions[3]);
+                advance = true;
+            }
+
+            if (advance) {
+                body.avancarAnimacao();
+            } else {
+                body.setAnimacao(animations.stopped);
+                body.recomecarAnimacao();
+            }
+
+            break;
+        case 1: // Cross Arrow
+
+            if (gTeclado.segurando[up]) {
+                pos_y -= speed;
+                body.setAnimacao(animations.up);
+                body.setInverterY(inversions[0]);
+                body.avancarAnimacao();
+
+            } else if (gTeclado.segurando[down]) {
+                pos_y += speed;
+                body.setAnimacao(animations.down);
+                body.setInverterY(inversions[1]);
+                body.avancarAnimacao();
+
+            } else if (gTeclado.segurando[left]) {
+                pos_x -= speed;
+                body.setAnimacao(animations.left);
+                body.setInverterX(inversions[2]);
+                body.avancarAnimacao();
+
+            } else if (gTeclado.segurando[right]) {
+                pos_x += speed;
+                body.setAnimacao(animations.right);
+                body.setInverterX(inversions[3]);
+                body.avancarAnimacao();
+
+            } else {
+                body.setAnimacao(animations.stopped);
+                body.recomecarAnimacao();
+            }
+
+            break;
+    }
+}
+
+void Movement::animationAdjustment(int set_stopped, int set_up, int set_down, int set_left, int set_right, bool *invert)
+{
+    if (set_stopped != 0) {
+        animations.stopped= set_stopped;
     }
 
-    if (gTeclado.segurando[down]) {
-        pos_y += speed;
+    if (set_up != 0) {
+        animations.up = set_up;
     }
 
-    if (gTeclado.segurando[left]) {
-        pos_x -= speed;
+    if (set_down != 0) {
+        animations.down = set_down;
     }
 
-    if (gTeclado.segurando[right]) {
-        pos_x += speed;
+    if (set_left != 0) {
+        animations.left = set_left;
+    }
+
+    if (set_right != 0) {
+        animations.right = set_right;
+    }
+
+    for (int count = 0; count < invert_size; count++) {
+        inversions[count] = invert[count];
     }
 }
